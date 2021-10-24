1b5e06da3fd0afa7425fa0176410d747c3400249
mrposeidonx <mrposedonx@github.com>
2021-10-24 12:09:21 +0700

    progress

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -278,12 +278,12 @@ var Icon$d = function (props) {
 
 var Icon$e = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 25 24" }, props),
-        React.createElement("path", { d: "M19.2 3H5.19995C4.09995 3 3.19995 3.9 3.19995 5V19C3.19995 20.1 4.09995 21 5.19995 21H19.2C20.3 21 21.2 20.1 21.2 19V5C21.2 3.9 20.3 3 19.2 3ZM19.2 19H5.19995V5H19.2V19Z", fill: "#b8add2" }),
-        React.createElement("path", { d: "M11.45 7.72021H6.44995V9.22022H11.45V7.72021Z", fill: "#b8add2" }),
-        React.createElement("path", { d: "M18.2 15.75H13.2V17.25H18.2V15.75Z", fill: "#b8add2" }),
-        React.createElement("path", { d: "M18.2 13.25H13.2V14.75H18.2V13.25Z", fill: "#b8add2" }),
-        React.createElement("path", { d: "M8.19995 18H9.69995V16H11.7V14.5H9.69995V12.5H8.19995V14.5H6.19995V16H8.19995V18Z", fill: "#b8add2" }),
-        React.createElement("path", { d: "M14.29 10.95L15.7 9.54L17.11 10.95L18.17 9.89L16.76 8.47L18.17 7.06L17.11 6L15.7 7.41L14.29 6L13.23 7.06L14.64 8.47L13.23 9.89L14.29 10.95Z", fill: "#b8add2" })));
+        React.createElement("path", { d: "M19.2 3H5.19995C4.09995 3 3.19995 3.9 3.19995 5V19C3.19995 20.1 4.09995 21 5.19995 21H19.2C20.3 21 21.2 20.1 21.2 19V5C21.2 3.9 20.3 3 19.2 3ZM19.2 19H5.19995V5H19.2V19Z", fill: "#dad2ec" }),
+        React.createElement("path", { d: "M11.45 7.72021H6.44995V9.22022H11.45V7.72021Z", fill: "#dad2ec" }),
+        React.createElement("path", { d: "M18.2 15.75H13.2V17.25H18.2V15.75Z", fill: "#dad2ec" }),
+        React.createElement("path", { d: "M18.2 13.25H13.2V14.75H18.2V13.25Z", fill: "#dad2ec" }),
+        React.createElement("path", { d: "M8.19995 18H9.69995V16H11.7V14.5H9.69995V12.5H8.19995V14.5H6.19995V16H8.19995V18Z", fill: "#dad2ec" }),
+        React.createElement("path", { d: "M14.29 10.95L15.7 9.54L17.11 10.95L18.17 9.89L16.76 8.47L18.17 7.06L17.11 6L15.7 7.41L14.29 6L13.23 7.06L14.64 8.47L13.23 9.89L14.29 10.95Z", fill: "#dad2ec" })));
 };
 
 var Icon$f = function (props) {
@@ -449,10 +449,10 @@ var Icon$A = function (props) {
         React.createElement("defs", null,
             React.createElement("linearGradient", { id: "paint0_linear", x1: "40.9849", y1: "37.4118", x2: "31.978", y2: "71.0261", gradientUnits: "userSpaceOnUse" },
                 React.createElement("stop", { stopColor: "#53DEE9" }),
-                React.createElement("stop", { offset: "1", stopColor: "#b8add2" })),
+                React.createElement("stop", { offset: "1", stopColor: "#dad2ec" })),
             React.createElement("linearGradient", { id: "paint1_linear", x1: "36.4919", y1: "19.505", x2: "40.1154", y2: "33.028", gradientUnits: "userSpaceOnUse" },
                 React.createElement("stop", { stopColor: "#53DEE9" }),
-                React.createElement("stop", { offset: "1", stopColor: "#b8add2" })),
+                React.createElement("stop", { offset: "1", stopColor: "#dad2ec" })),
             React.createElement("clipPath", { id: "clip0" },
                 React.createElement("rect", { width: "80", height: "80", fill: "white", transform: "matrix(-1 0 0 1 80 0)" })))));
 };
@@ -2103,7 +2103,7 @@ var links = [
             },
             {
                 label: "Docs",
-                href: "https://defisapphire.gitbook.io/sapphire-defi/",
+                href: "https://docs.witcherdefi.com/",
             },
             {
                 label: "Blog",
@@ -2116,12 +2116,12 @@ var socials = [
     {
         label: "Telegram",
         icon: "TelegramIcon",
-		href: "https://t.me/DefiSapphire",
+		href: "https://t.me/witcherdefi",
     },
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/DefiSapphire",
+        href: "https://twitter.com/WitcherFi",
     },
 ];
 var MENU_HEIGHT = 64;
@@ -2698,16 +2698,16 @@ var templateObject_1$K;
 var baseColors = {
     failure: "#ED4B9E",
     primary: "#c5211c",
-    primaryBright: "#b8add2",
-    primaryDark: "#b8add2",
-    secondary: "#b8add2",
+    primaryBright: "#dad2ec",
+    primaryDark: "#dad2ec",
+    secondary: "#dad2ec",
     success: "#31D0AA",
     warning: "#FFB237",
 };
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#f9f7f4", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#27262c", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#b8add2", textDisabled: "#b8add2", textSubtle: "#b8add2", borderColor: "#E9EAEB", card: "#27262cd4", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#f9f7f4", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#27262c", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#dad2ec", textDisabled: "#dad2ec", textSubtle: "#dad2ec", borderColor: "#E9EAEB", card: "#27262cd4", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
 var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#fb4039", background: "#4b0400", backgroundDisabled: "#6d0803", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#560f0b", primaryDark: "#0098A1", tertiary: "#6d0803", text: "#fb4039", textDisabled: "#fb4039", textSubtle: "#fb4039", borderColor: "#524B63", card: "#480806", gradients: {
